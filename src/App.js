20a810efd0561f325ef13589effd68e26d88234d
DarkVader121 <128356705+DarkVader121@users.noreply.github.com>
2023-07-15 10:56:19 -0400

    meta tags in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta name="description" content="This is the Lilttle Lemon Restaurant"/>
+        <meta name="og:title" content="Little Lemon"/>
+        <meta name="og:description" content="This is the Little Lemon Restaurant"/>
+        <meta name="ig:image" content=""/>
+      </Helmet>
     <p>This is the starting page of Little Lemon Project</p>
     </div>
   );
